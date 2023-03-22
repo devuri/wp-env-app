9dcae97be50dd16df11e8b7411e9e6fa8c72c49d
Uriel Wilson <devuri@users.noreply.github.com>
2023-03-22 14:16:29 -0500

    Update app.php

--- app.php
+++ app.php
@@ -1,32 +1,70 @@
 <?php
 
-use DevUri\Config\Setup;
+use DevUri\Config\Kernel;
 
+/*
+ * This is the bootstrap file for the web application.
+ *
+ * It loads the necessary files and sets up the environment for the application to run.
+ * This includes initializing the Composer autoloader, which is used to load classes and packages.
+ *
+ * The application uses Composer to manage dependencies and packages including WordPress themes and plugins, which allows you to easily add, update, and remove packages as needed.
+ * To install or update packages, you can use the `composer` command line.
+ *
+ * IMPORTANT: Do NOT modify this file unless you are sure of what you are doing.
+ * Any changes you make to this file may affect the behavior of the entire application and cause errors or unexpected behavior.
+ * If you are unsure about how to modify the application, please refer to the documentation or seek assistance from a qualified developer.
+ *
+ * To modify the setup, please refer to the documentation for instructions.
+ *
+ */
 if ( file_exists( \dirname( __FILE__ ) . '/vendor/autoload.php' ) ) {
     require_once \dirname( __FILE__ ) . '/vendor/autoload.php';
 } else {
     exit("Cant find the vendor autoload file.");
 }
 
+/**
+ * Start the web application.
+ *
+ * @var Kernel
+ */
+$http_app = new Kernel( __DIR__, [
+    'web_root'      => 'public',
+    'content_dir'   => 'app',
+    'plugin_dir'    => 'app/plugins',
+    'mu_plugin_dir' => 'app/mu-plugins',
+    'default_theme' => 'brisko',
+] );
+
+/*
+ * Load constant overrides.
+ *
+ * This will load constant values that override constants defined in setup.
+ */
+$http_app->overrides();
+
+/*
+ * Configuration settings for your web application.
+ *
+ * We recommend using the .env file to set these values.
+ * The possible values are: 'debug', 'development', 'staging', 'production', or 'secure'.
+ * The web application will use either the value of WP_ENVIRONMENT_TYPE or 'production'.
+ *
+ * Error handler: the framework allows for the use of oops or symfony.
+ * The default error handler is symfony, to change the handler use `init(['errors' => 'oops'])`
+ * to disable the handlers completely use `init(['errors' => null])`
+ * error handler only run in 'debug', 'development' or 'local'.
+ *
+ * If you want to set the environment to 'development', add the following to your .env
+ * WP_ENVIRONMENT_TYPE=development
+ */
+$http_app->init();
 
 /**
- * define public web root dir.
- */
-define('PUBLIC_WEB_ROOT', __DIR__ . '/public');
-
-// Custom Directory PATH.
-define('APP_DIR', '/content' );
-define('WP_CONTENT_DIR', PUBLIC_WEB_ROOT . APP_DIR );
-
-// Custom Content Directory URL.
-define('CONTENT_DIR', APP_DIR );
-define('WP_CONTENT_URL', env('WP_HOME') . CONTENT_DIR );
-
-/**
- * The base configuration for WordPress
+ * WordPress Database Table prefix.
  *
- * @link https://codex.wordpress.org/Editing_wp-config.php
- *
- * @package WordPress
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
  */
-Setup::init(__DIR__)->config(); // production
+$table_prefix = env('DB_PREFIX');
