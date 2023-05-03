4639ea2b996f02a8ca0b4e25d1da1b710f93c220
devuri <devuri@users.noreply.github.com>
2023-05-02 19:23:53 -0500

    feat: Adds storage dir

---
+++ storage/maintenance.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Web Application</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  </head>
+    <body class="antialiased">
+        <div class="bg-dark text-secondary px-4 py-5 text-center">
+		    <div class="py-5">
+		      <h1 class="display-5 fw-bold text-white">Maintenance mode</h1>
+		      <div class="col-lg-6 mx-auto">
+		        <p class="fs-5 mb-4">We're working on this</p>
+		        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+		          <!-- <button type="button" class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">Custom button</button>
+		          <button type="button" class="btn btn-outline-light btn-lg px-4">Secondary</button> -->
+		        </div>
+		      </div>
+		    </div>
+		  </div>
+    </body>
+</html>
