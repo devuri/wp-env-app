80414c89ae3848f32ade6ddaedcba6f4fc713365
Uriel <devuri@users.noreply.github.com>
2023-06-18 00:35:34 -0500

    Update wp-config.php

--- public/wp-config.php
+++ public/wp-config.php
@@ -2,15 +2,6 @@
 
 require_once dirname( __DIR__ ) . '/bootstrap.php';
 
-/**
- * WordPress Database Table prefix.
- *
- * You can have multiple installations in one database if you give each
- * a unique prefix. Only numbers, letters, and underscores please!
- */
-$table_prefix = env('DB_PREFIX');
-
-
 if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
 }
