91c10476ccc9ad794311033a9371ef151171bc0d
devuri <devuri@users.noreply.github.com>
2023-09-08 12:22:22 -0500

    feat: adds a way to check if we are in maintenance mode

--- storage/maintenance.php
+++ storage/maintenance.php
@@ -11,7 +11,7 @@
 		    <div class="py-5">
 		      <h1 class="display-5 fw-bold text-white">Maintenance mode</h1>
 		      <div class="col-lg-6 mx-auto">
-		        <p class="fs-5 mb-4">We're working on this</p>
+		        <p class="fs-5 mb-4">We are currently performing scheduled maintenance. Please check back soon.</p>
 		        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
 		          <!-- <button type="button" class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">Custom button</button>
 		          <button type="button" class="btn btn-outline-light btn-lg px-4">Secondary</button> -->
