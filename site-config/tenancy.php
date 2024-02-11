50691a570713addedf831509dad55694ef440bb2
Uriel <devuri@users.noreply.github.com>
2024-02-11 00:43:05 -0500

    Update and rename tenancy.php to tenancy.php

--- site-config/tenancy.php
+++ site-config/tenancy.php
@@ -6,9 +6,9 @@
  * This constant should be assigned the UUID value of the primary tenant that acts as the landlord.
  * Setting this value is crucial for identifying the main tenant in a multi-tenant configuration.
  */
-define('LANDLORD_UUID', null);
+\define( 'LANDLORD_UUID', null );
 
-/**
+/*
  * Controls the activation of multi-tenant capabilities within the WordPress environment.
  *
  * When enabled, this feature allows the operation of multiple WordPress sites from a single installation,
@@ -18,4 +18,4 @@ define('LANDLORD_UUID', null);
  * For comprehensive guidance on configuring a multi-tenant environment, refer to the official documentation:
  * @link https://devuri.github.io/wp-env-config/multi-tenant/
  */
-define('ALLOW_MULTITENANT', false);
+\define( 'ALLOW_MULTITENANT', true );
